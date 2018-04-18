126f2e8f6138a125ace5bdc0554cfef61dd4c1a3
K-1 <138109029@qq.com>
2018-04-18 16:16:56 +0800

    vb.net10  excel 生成模版

---
+++ Test-Data/Form1.Designer.vb
@@ -0,0 +1,215 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("楷体", 10.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Button1.Location = New System.Drawing.Point(148, 23)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(70, 36)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "导入"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(430, 12)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(164, 89)
+        Me.RichTextBox1.TabIndex = 2
+        Me.RichTextBox1.Text = ""
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(26, 165)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(192, 23)
+        Me.ProgressBar1.TabIndex = 3
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Font = New System.Drawing.Font("楷体", 10.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(26, 17)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(104, 95)
+        Me.GroupBox1.TabIndex = 5
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "随机数范围"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("楷体", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(51, 56)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(45, 26)
+        Me.TextBox2.TabIndex = 3
+        Me.TextBox2.Text = "0.5"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("楷体", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(51, 26)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(45, 26)
+        Me.TextBox1.TabIndex = 3
+        Me.TextBox1.Text = "-0.5"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("楷体", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label2.Location = New System.Drawing.Point(10, 58)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(40, 16)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "max:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("楷体", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label1.Location = New System.Drawing.Point(10, 31)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(40, 16)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "min:"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(489, 261)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(61, 30)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "取消"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("楷体", 10.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Button2.Location = New System.Drawing.Point(148, 70)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(70, 39)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "退出"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Checked = True
+        Me.CheckBox1.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox1.Font = New System.Drawing.Font("宋体", 7.5!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.CheckBox1.Location = New System.Drawing.Point(144, 133)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(74, 14)
+        Me.CheckBox1.TabIndex = 8
+        Me.CheckBox1.Text = "生成后重启"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Font = New System.Drawing.Font("楷体", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.TextBox3.Location = New System.Drawing.Point(77, 124)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(45, 26)
+        Me.TextBox3.TabIndex = 9
+        Me.TextBox3.Text = "0"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("楷体", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label3.Location = New System.Drawing.Point(29, 126)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(48, 16)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "孔位:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(243, 214)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Location = New System.Drawing.Point(150, 200)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.Manual
+        Me.Text = "数据处理"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+
+End Class
---
+++ Test-Data/Form1.vb
@@ -0,0 +1,269 @@
+﻿Public Class Form1
+    Dim xlApp, xlBook, xlSheet, xlBook_new, xlsheet_new, xlapp_new As Object
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim xlApp, xlBook, xlSheet, xlBook_new, xlsheet_new, xlapp_new As Object
+        Dim A_(58) As Integer
+        Dim B_(), C_(), D_(), CD_(), E_(), F_() As Double
+        Dim i, sig As Integer
+        Dim strFile As String = ""
+        Dim intRow, intCol As Integer
+        Dim lastRow, lastCol As Integer
+        Dim retry As Boolean
+        Dim minRnd, maxRnd, Rndi As Double
+        Dim str_in As String
+        Dim strDtm As Date
+        Dim count As Integer = 0
+
+        '可变
+        'intRow = 59
+        'intCol = 1
+        With OpenFileDialog1
+            .Filter = "表格文件(*.xls)|*.xls;*.xlsx"
+            .Title = "数据导入"
+            .FileName = ""
+        End With
+        If OpenFileDialog1.ShowDialog = System.Windows.Forms.DialogResult.OK Then
+            Button1.Enabled = False
+            'Try
+            '导入表格
+            xlApp = CreateObject("Excel.Application")
+            xlBook = xlApp.Workbooks.Open(OpenFileDialog1.FileName)
+            'xlApp.Visible = True '设置EXCEL对象可见（或不可见）  
+            xlSheet = xlBook.Worksheets(1) '设置活动工作表''
+
+            xlapp_new = CreateObject("Excel.Application")
+            xlBook_new = xlapp_new.workbooks.add
+            'xlapp_new.Visible = True
+            lastCol = xlSheet.usedrange.columns.count
+
+            lastRow = xlSheet.usedrange.rows.count
+            'lastRow = xlSheet.cells(lastRow, 3).end(xlup).row
+            ReDim B_(lastRow - 1), C_(lastRow - 1), D_(lastRow - 1), CD_(lastRow - 1), E_(lastRow - 1), F_(lastRow)
+            F_(lastRow) = 0
+            'intCol = 1
+            ProgressBar1.Minimum = 0
+            ProgressBar1.Maximum = lastCol
+            '添加sheet页
+            retry = True
+
+            While retry
+                Try
+                    'Do While Not (String.IsNullOrEmpty(xlSheet.cells(1, intCol).value.ToString()))
+                    'Loop
+                    For j = 1 To lastCol
+                        xlsheet_new = xlBook_new.Worksheets.Add
+                        intCol = j
+                    Next
+                    retry = False
+                    count = 0
+                Catch ex As Exception
+                    RichTextBox1.Text = "0: addSheet col-" + intCol + " ,row-" + i + vbCrLf + RichTextBox1.Text
+                    retry = True
+                    count = count + 1
+                    If count > 100 Then
+                        xlapp_new.Visible = True
+                        MessageBox.Show("输出错误！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                        Exit Sub
+                    End If
+                End Try
+            End While
+            RichTextBox1.Text = "添加表格完成！" + vbCrLf + RichTextBox1.Text
+
+            For j = 1 To lastCol
+                Try
+                    Rndi = Rndz(minRnd, maxRnd)
+                Catch ex As Exception
+                    MessageBox.Show("请输入正确的随机数范围！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    Exit Sub
+                End Try
+                xlsheet_new = xlBook_new.worksheets(j)       '表格个数
+
+                If IsDate(xlSheet.cells(1, j).value) Then
+                    strDtm = xlSheet.cells(1, j).value
+                    str_in = Format(strDtm, "yyyy-mm-dd") '''''''''''''''''''''''''''''''''''弃掉
+                    str_in = xlSheet.cells(1, j).value
+
+                    str_in = "#" + str_in.Split("/")(1) + "月" + str_in.Split("/")(2) + "日" + Rnd_hour.ToString("00") + "时" + Rnd_Minute.ToString() + "分" + Rnd_Minute.ToString() + "秒"
+
+                Else
+                    '  [] *
+                    str_in = "#" + xlSheet.cells(1, j).value + "___" + Rnd_hour.ToString("00") + "时" + Rnd_Minute.ToString() + "分" + Rnd_Minute.ToString() + "秒"
+                    str_in = str_in.Replace(",", "_")
+                    str_in = str_in.Replace("/", "|")
+                    str_in = str_in.Replace("\", "|")
+                    str_in = str_in.Replace("?", "a")
+                    str_in = str_in.Replace("*", "&")
+                    str_in = str_in.Replace("[", "(")
+                    str_in = str_in.Replace("]", ")")
+                End If
+
+                str_in = TextBox3.Text + str_in
+                If str_in.Length > 32 Then
+                    str_in = str_in.Substring(0, 32)
+                End If
+                xlsheet_new.name = str_in     '表格名字  '！不能重名
+                retry = True
+                While retry
+                    Try
+                        ''''''''''''''''''''''''''''
+                        '''''''''''''''''''''''''
+                        For i = lastRow - 1 To 1 Step -1
+                            F_(i) = xlSheet.cells(i + 1, j).value
+                            E_(i) = Math.Round(F_(i) - F_(i + 1), 2)
+                            sig = Math.Sign(E_(i))   '????????两位小数
+                            C_(i) = Math.Round(E_(i) + Rndi, 2)
+                            'MessageBox.Show(C_(i)
+                            D_(i) = Math.Round(C_(i) - 2 * E_(i), 2)
+                            F_(i) = Math.Round(F_(i), 2)
+                        Next
+
+                        xlsheet_new.cells(1, 1).value = "测点"
+                        xlsheet_new.cells(1, 2).value = "深度(米)"
+                        xlsheet_new.cells(1, 3).value = "A+(毫米)"
+                        xlsheet_new.cells(1, 4).value = "A-(毫米)"
+                        xlsheet_new.cells(1, 5).value = "测值(毫米)"
+                        xlsheet_new.cells(1, 6).value = "累计位移(毫米)"
+
+                        For i = 1 To lastRow - 1
+                            xlsheet_new.cells(i + 1, 1).value = i
+                            xlsheet_new.cells(i + 1, 2).value = i / 2
+                            xlsheet_new.cells(i + 1, 3).value = C_(i).ToString()
+                            xlsheet_new.cells(i + 1, 4).value = D_(i).ToString()
+                            xlsheet_new.cells(i + 1, 5).value = E_(i).ToString()
+                            xlsheet_new.cells(i + 1, 6).value = F_(i).ToString()
+                        Next i
+                        retry = False
+                        ProgressBar1.Value = j
+                        My.Application.DoEvents()
+                        RichTextBox1.Text = j.ToString + vbCrLf + RichTextBox1.Text
+                        count = 0
+                    Catch ex As Exception
+                        RichTextBox1.Text = "1: name col-" + j.ToString + " ,row-" + i.ToString + vbCrLf + RichTextBox1.Text
+                        retry = True
+                        count = count + 1
+                        If count > 100 Then
+                            xlapp_new.Visible = True
+                            MessageBox.Show("输出错误！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                            Exit Sub
+                        End If
+                    End Try
+                End While
+
+                'intCol = intCol + 1
+                'Loop
+            Next j
+
+
+
+            Button1.Enabled = True
+            xlSheet = Nothing
+            xlBook.Close()
+            xlApp.Quit()
+            xlBook = Nothing
+
+
+            xlsheet_new = Nothing
+
+            xlBook_new = Nothing
+            xlapp_new.Visible = True
+            xlApp = Nothing
+            xlapp_new = Nothing
+            ProgressBar1.Value = 0
+            If CheckBox1.Checked = True Then
+                Application.Restart()
+            End If
+        End If
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Try
+            xlSheet = Nothing
+            xlBook.Close()
+            xlApp.Quit()
+            xlBook = Nothing
+
+            xlsheet_new = Nothing
+
+            xlBook_new = Nothing
+            xlapp_new.visible = True
+            xlapp_new.Quit()
+            xlApp = Nothing
+            xlapp_new = Nothing
+        Catch ex As Exception
+
+        End Try
+        
+        Application.Exit()
+
+
+    End Sub
+
+
+    Private Function Rndz(ByVal a_min As Single, ByVal b_max As Single) As Double '
+        '-50   50
+        a_min = CInt(a_min * 100)
+        b_max = CInt(b_max * 100)
+
+        Return Int((a_min - b_max + 1) * Rnd() + b_max) / 100
+        'M = (Int(Rnd() * 101) - 51(0 - 100) - 50~50
+
+    End Function
+    Private Function randomizer() As Double
+        Dim A As Integer
+        Dim M As Double
+        'Randomiz
+        A = Int(Rnd() * 1000) + 1    '0.01-0.50
+        Select Case A
+            Case 1 To 10
+                M = (Int(Rnd() * 50) + 1) / 100  '0.01-0.50
+            Case 11 To 950
+                M = (Int(Rnd() * 40) + 10) / 100  '大概率 0.10-0.50
+            Case 951 To 1000
+                M = (Int(Rnd() * 50) + 1) / 100
+        End Select
+        Return M
+    End Function
+    Private Function Rnd_hour() As Double
+        Dim A, M As Integer
+        'Randomiz
+        A = Int(Rnd() * 2) + 1    '0.01-0.50
+        Select Case A
+            Case 1
+                M = (Int(Rnd() * 5) + 8)  '[8,12]
+            Case 2
+                M = (Int(Rnd() * 5) + 13)  '[14,18]
+        End Select
+        Return M
+    End Function
+    Private Function Rnd_Minute() As Double
+        Dim A As Integer
+        'Randomiz
+        A = Int(Rnd() * 61)    '0.01-0.50
+        Return A
+    End Function
+
+    Private Sub CheckBox1_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CheckBox1.CheckedChanged
+
+    End Sub
+    Private Function strToSheetName(ByVal str As String) As String  'Test  未使用
+        Dim Dtm As Date
+        If IsDate(str) Then
+            Dtm = str
+            str = "#" + Dtm.Month.ToString + "月" + Dtm.Day.ToString + "日" + Rnd_hour.ToString("00") + "时" + Rnd_Minute.ToString() + "分" + Rnd_Minute.ToString() + "秒"
+        Else
+            str = "#" + str + "___" + Rnd_hour.ToString("00") + "时" + Rnd_Minute.ToString() + "分" + Rnd_Minute.ToString() + "秒"
+            str = str.Replace(",", "_")
+            str = str.Replace("/", "|")
+            str = str.Replace("\", "|")
+            str = str.Replace("?", "a")
+            str = str.Replace("*", "&")
+            str = str.Replace("[", "(")
+            str = str.Replace("]", ")")
+        End If
+
+        Return str
+    End Function
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
